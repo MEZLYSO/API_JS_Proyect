bb840bb5a50fe4653b078d14e09f67654b7df1cc
Maury SO <lynuxazura@gmail.com>
2025-06-17 02:28:47 -0600

    Init 1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import express from 'express'
 
 app = express()
 
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hi" })
+})
+
 app.listen(3000, () => {
   console.log("Serive running in port 3000")
 })
