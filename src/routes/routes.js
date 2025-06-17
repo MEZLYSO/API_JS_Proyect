1d078d3f8e94891edf95ab57a866e34f8418583f
Maury SO <lynuxazura@gmail.com>
2025-06-17 03:50:24 -0600

    Minimal

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,11 @@ const userRouter = require('./userRoutes.js')
 
 const routes = Router()
 
+//Message Default
+routes.get("/", (req, res) => {
+  res.json({ message: "Hi not use this API, please :P " })
+})
+//Routes 
 routes.use("/user", userRouter)
 
 module.exports = routes
