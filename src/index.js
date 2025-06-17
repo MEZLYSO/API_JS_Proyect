48fbb40232a27115116038f6651bff02cd5a0d81
Maury SO <lynuxazura@gmail.com>
2025-06-17 05:06:36 -0600

    Minimal changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
+const cors = require('cors')
 const express = require('express')
 const routes = require('./routes/routes')
 
 app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
 //Message default
-app.get("/", (req, res) => {
-  res.json({ message: "Hi not use this API, please :P " })
+app.get("/", (_req, res) => {
+  res.json({ message: "Hi, not use this API, please :P " })
 })
 
 const PORT = process.env.PORT || 3000
