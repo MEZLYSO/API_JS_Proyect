f1c2f270b1e2ffcc276ef88e91ab659aef81c9b2
Maury SO <lynuxazura@gmail.com>
2025-06-18 04:31:18 -0600

    New SQL

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -2,7 +2,7 @@ const db = require('../database/db')
 
 class userService {
   async getUser(username, password) {
-    const user = await db.query("SELECT * FROM Users WHERE Username=? AND Userpassword=?", [username, password])
+    const user = await db.query("SELECT * FROM Users u INNER JOIN Participants p ON u.IDUser = p.IDUser WHERE u.Username = ? AND u.Userpassword = ? ", [username, password])
     if (user.length > 0) {
       return user
     } else {
